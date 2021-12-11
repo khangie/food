3deda7c7e4f5abe0de01bd172f406825e601cf72
Khang Nguyen <knguyen@Khangs-MBP.lan>
2021-12-11 14:45:34 -0800

    code cleanup

--- App.js
+++ App.js
@@ -8,33 +8,8 @@ const navigator = createStackNavigator({
 {
   initialRouteName: 'Search',
   defaultNavigationOptions: {
-    title: 'BusinessSearch'
+    title: 'Business Search'
   }
 });
 
 export default createAppContainer(navigator);
-
-/*
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-*/
