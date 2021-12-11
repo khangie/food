786a7eea9f62fe1d9d589ba73eb35d96bc5bcf1b
Khang Nguyen <knguyen@Khangs-MBP.lan>
2021-12-11 14:43:27 -0800

    first commit

--- App.js
+++ App.js
@@ -1,3 +1,20 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import SearchScreen from './src/screens/SearchScreen';
+
+const navigator = createStackNavigator({
+  Search: SearchScreen
+}, 
+{
+  initialRouteName: 'Search',
+  defaultNavigationOptions: {
+    title: 'BusinessSearch'
+  }
+});
+
+export default createAppContainer(navigator);
+
+/*
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
@@ -19,3 +36,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+*/
