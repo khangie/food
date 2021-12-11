7ac11e7a811ff5db3261e4771bf6ef4f2f4d9d12
Khang Nguyen <knguyen@Khangs-MBP.lan>
2021-12-11 15:31:00 -0800

    added state to SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange }) => {
     return (
         <View style={styles.backgroundStyle}>
             <Feather name="search" style={styles.iconStyle}/>
-            <TextInput style={styles.inputStyle}
+            <TextInput 
+                autoCapitalize="none"
+                autoCorrect={false}
+                style={styles.inputStyle}
                 placeholder="search"
+                value={term}
+                onChangeText={newTerm => onTermChange(newTerm)}
             />
         </View>
     );
